e9c9af79e8130a78d426d4105653de185af0719c
sephpace <sephpace@gmail.com>
2019-01-15 17:00:23 -0700

    Setup DialogMenu class and functions

---
+++ NPC/DialogMenu.py
@@ -0,0 +1,11 @@
+
+
+
+class DialogMenu:
+    def __init__(self, options):
+        self.__options = options
+
+    def get_options(self): return self.__options
+
+    def print(self):
+        pass
