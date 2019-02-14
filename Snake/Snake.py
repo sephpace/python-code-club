2819a9bb58d992cd609286516a71505972054e3a
sephpace <sephpace@gmail.com>
2019-02-14 14:33:45 -0700

    setup Snake.py with a constructor, getters, and setters

---
+++ Snake/Snake.py
@@ -0,0 +1,16 @@
+
+class Snake:
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    def get_x(self): return self.__x
+
+    def get_y(self): return self.__y
+
+    def set_x(self, x): self.__x = x
+
+    def set_y(self, y): self.__y = y
+
+    def draw(self, surface):
+        pass
