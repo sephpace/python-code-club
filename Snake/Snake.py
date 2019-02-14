dd01668ac190ad73ee0b7e44a4d4791e95c13eea
sephpace <sephpace@gmail.com>
2019-02-14 15:16:57 -0700

    add get_pos() and set_pos() functions and remove unnecessary functions.

--- Snake/Snake.py
+++ Snake/Snake.py
@@ -4,18 +4,19 @@ from GUI import GUI
 
 
 class Snake:
-    def __init__(self, x, y, color):
-        self.__x = x
-        self.__y = y
+    def __init__(self, x, y, color, direction=0):
+        self.__pos = [(x * GUI.GRID_SIZE, y * GUI.GRID_SIZE)]
         self.__color = color
+        self.__direction = direction
 
-    def get_x(self): return self.__x
+    def get_pos(self): return self.__pos[0]
 
-    def get_y(self): return self.__y
-
-    def set_x(self, x): self.__x = x
-
-    def set_y(self, y): self.__y = y
+    def set_pos(self, x, y):
+        for i in range(len(self.__pos), 0, -1):
+            if i == 0:
+                self.__pos[0] = (x, y)
+            else:
+                self.__pos[i] = self.__pos[i - 1]
 
     def draw(self, surface):
-        draw.rect(surface, self.__color, (self.__x, self.__y, GUI.GRID_SIZE, GUI.GRID_SIZE))
+        draw.rect(surface, self.__color, (self.__pos[0][0], self.__pos[0][1], GUI.GRID_SIZE, GUI.GRID_SIZE))
