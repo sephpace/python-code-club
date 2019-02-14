08482f7ba6f42f8228100bfca1ed74f7816583b0
sephpace <sephpace@gmail.com>
2019-02-14 15:19:36 -0700

    add Snake object to display and edit GRID_SIZE.

--- Snake/GUI.py
+++ Snake/GUI.py
@@ -1,18 +1,26 @@
 
 import pygame
 
+from Snake import Snake
+
 
 class GUI:
+    # Size constants
     SCREEN_SIZE = 500
-    GRID_SIZE = SCREEN_SIZE / 100
+    GRID_SIZE = 20
 
     def __init__(self):
         pygame.init()
+
         self.__display = pygame.display.set_mode((self.SCREEN_SIZE, self.SCREEN_SIZE))
 
         pygame.display.update()  # Implement double-buffering, etc.
 
         self.__clock = pygame.time.Clock()
+
+        # Add game objects
+        self.__game_objects = []
+        self.__game_objects.append(Snake(12, 9, (0, 0, 255)))
 
     def start(self):
         # -- Main Loop --
