a4b192783767da9005d5eada7390689be3e41c59
Cache Makers <Image15@iMac-H.local>
2019-02-20 19:54:59 -0700

    create player class

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,28 +1,62 @@
 
+import math
 import pygame
 
 
-pygame.init()
+# --- Classes ---
+class Player:
+    def __init__(self, pos, rot=math.pi / 2, color=(255, 255, 255)):
+        self.set_pos(pos)
+        self.__rot = rot
+        self.__color = color
+        self.__points = ()
 
-screen = pygame.display.set_mode((500, 500))
+    def set_pos(self, pos):
+        self.__points = ((pos[0], pos[1] - 5), (pos[0] + 5, pos[1] + 10), (pos[0], pos[1] + 5), (pos[0] - 5, pos[1] + 10), (pos[0], pos[1] - 5))
 
-clock = pygame.time.Clock()
+    def set_rot(self, rad):
+        pass
 
-# --- Main Loop ---
-while True:
-    # Handle events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+    def draw(self, surface):
+        pygame.draw.polygon(surface, self.__color, self.__points)
 
-    # Clear the screen
-    screen.fill((0, 0, 0))
 
-    # Draw the player
+# --- Functions ---
+def start():
+    run()
 
-    # Update the screen
-    pygame.display.update()
 
-    # Set clock speed
-    clock.tick(60)
+def run():
+    """Main Loop"""
+    while True:
+        # Handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+        # Clear the screen
+        screen.fill((0, 0, 0))
+
+        # Draw the player
+        player.draw(screen)
+
+        # Update the screen
+        pygame.display.update()
+
+        # Set clock speed
+        clock.tick(60)
+
+
+if __name__ == '__main__':
+    pygame.init()
+
+    SCREEN_SIZE = 500
+
+    screen = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
+
+    clock = pygame.time.Clock()
+
+    player = Player((SCREEN_SIZE // 2, SCREEN_SIZE // 2))
+
+    start()
