a653b38d0f804fc50f9b0b82738df33c96da09f6
sephpace <sephpace@gmail.com>
2019-02-26 16:42:01 -0700

    make it so player can move up and down with the arrow keys

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -6,18 +6,23 @@ import pygame
 # --- Classes ---
 class Player:
     def __init__(self, pos, speed=0, rot=(3 * math.pi / 2), color=(255, 255, 255)):
-        self.__points = ()
-        self.__pos = pos
-        self.__speed = speed
-        self.__rot = rot
-        self.__color = color
+        self.__points = ()       # The points that make up the player's shape
+        self.__pos = pos         # The player's position
+        self.__speed = speed     # The speed of player
+        self.__rot = rot         # The direction the player is facing
+        self.__color = color     # The color of the player
+        self.__go_left = False   # Whether the player is turning left or not
+        self.__go_right = False  # Whether the player is turning right or not
+        self.__direction = rot   # The direction the player is traveling
+
+        # Update the player's position
         self.__update_pos()
-        self.__go_left = False
-        self.__go_right = False
 
     def get_pos(self): return self.__pos
 
     def get_rot(self): return self.__rot
+
+    def get_speed(self): return self.__speed
 
     def set_pos(self, pos):
         self.__pos = pos
@@ -34,14 +39,25 @@ class Player:
 
     def set_right(self, right): self.__go_right = right
 
+    def move_forward(self):
+        player.set_speed(player.get_speed() + 1)
+        # TODO: To move forward when UP is pressed, add a unit-vector in the direction of self.__rot to the velocity vector
+
+    def move_backward(self):
+        player.set_speed(player.get_speed() - 1)
+
     def draw(self, surface):
         pygame.draw.polygon(surface, self.__color, self.__points)
 
     def handle(self):
+        # Turn left or right when turning
         if self.__go_left:
             self.set_rot(self.get_rot() - math.pi / 20)
         if self.__go_right:
             self.set_rot(self.get_rot() + math.pi / 20)
+
+        # Move in the correct direction
+        self.set_pos((self.__pos[0] + (self.__speed * math.cos(self.__direction)), self.__pos[1] + (self.__speed * math.sin(self.__direction))))
 
     def __update_pos(self):
         self.__points = ((self.__pos[0] + int(5 * math.cos(self.__rot)), self.__pos[1] + int(5 * math.sin(self.__rot))),  # Front
@@ -63,6 +79,10 @@ def run():
         for event in pygame.event.get():
             # If a key is pressed
             if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    player.move_forward()
+                if event.key == pygame.K_DOWN:
+                    player.move_backward()
                 if event.key == pygame.K_LEFT:
                     player.set_left(True)
                 if event.key == pygame.K_RIGHT:
